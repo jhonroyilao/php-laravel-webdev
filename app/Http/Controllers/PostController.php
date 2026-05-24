aef2cd7f80e53dc7530126af3ad1e3b9196f60d1
jhonroyilao <jhonroyilao@gmail.com>
2026-05-24 19:31:28 +0800

    fix postcontroller function logic: add edit and update functions

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,46 +8,82 @@ use Illuminate\Support\Facades\Log;
 
 class PostController extends Controller
 {
-    // display post page
+    // DISPLAY POSTS PAGE
     public function postPage()
     {
-        $allPosts = DB::table('post')
-        ->join('statuses', 'post.status', '=', 'statuses.id')
-        ->select(
-            'post.*',
-            'statuses.display_name as status_name'
-        )
-        ->get();
+        $posts = DB::table('post')
+            ->leftJoin('statuses', 'post.status', '=', 'statuses.id')
+            ->select(
+                'post.*',
+                'statuses.display_name as status_display_name'
+            )
+            ->get();
 
-        return view('posts', ['posts' => $allPosts]);
+        $statuses = DB::table('statuses')->get();
+
+        return view('posts', compact('posts', 'statuses'));
     }
 
-    // handle form
+    // DISPLAY EDIT FORM
+    public function editForm($id)
+    {
+        $post = DB::table('post')->where('id', $id)->first();
+
+        $statuses = DB::table('statuses')->get();
+
+        return view('edit', compact('post', 'statuses'));
+    }
+
+    // ADD POST
     public function addPost(Request $request)
     {
-        $request->validate([
-            'title' => ['required', 'min:2'],
-            'description' => ['required', 'min:5'],
-        ],
-        [
-            'title.required' => 'You need to add a title',
-            'title.min' => 'Title must be at least 2 characters long',
+        $request->validate(
+            [
+                'title' => ['required', 'min:2'],
+                'description' => ['required', 'min:5'],
+            ],
+            [
+                'title.required' => 'You need to add a title',
+                'title.min' => 'Title must be at least 2 characters long',
 
-            'description.required' => 'You need to add a description',
-            'description.min' => 'Description must be at least 5 characters long',
-        ]);
+                'description.required' => 'You need to add a description',
+                'description.min' => 'Description must be at least 5 characters long',
+            ]
+        );
 
-        // log only
-        Log::info('==== NEW POST====');
+        Log::info('==== NEW POST ====');
         Log::info('Title: ' . $request->title);
         Log::info('Description: ' . $request->description);
 
-        // fetch db record
-        $allPosts = DB::table('post')->get();
-
-        // return json
-        return response()->json([
-            'data' => $allPosts
+        DB::table('post')->insert([
+            'title' => $request->title,
+            'description' => $request->description,
+            'created_by' => 1,
+            'created_at' => now(),
+            'status' => $request->status,
         ]);
+
+        return redirect('posts');
+    }
+
+    // UPDATE POST
+    public function updatePost(Request $request, $id)
+    {
+        $request->validate(
+            [
+                'title' => ['required', 'min:2'],
+                'description' => ['required', 'min:5'],
+            ]
+        );
+
+        DB::table('post')
+            ->where('id', $id)
+            ->update([
+                'title' => $request->title,
+                'description' => $request->description,
+                'status' => $request->status,
+            ]);
+
+        return redirect('posts');
     }
 }
